705e20962c86b33ad2982faf2b12bde322c1ca8c
Fernando Murguia <fernando@openix.com.ar>
2022-01-17 12:23:44 -0300

    Agregado practica arrays

---
+++ C#_Sprint_1/arrays/arrays/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] strings = new String[5];
+
+            strings[4] = "test";
+            Console.WriteLine("posiciones disponibles:{0}", strings.Length);
+            Console.WriteLine("posicion 4:{0}", strings[4]);
+
+
+            int[] numbers = { 5, 1, 4, 2, 7 };
+            foreach (int number in numbers)
+            {
+                Console.WriteLine("valor {0}", number);
+
+            }
+
+
+
+        
+            // Declare and set array element values.
+            int[] array2 = new int[] { 1, 3, 5, 7, 9 };
+
+            // Alternative syntax.
+            int[] array3 = { 1, 2, 3, 4, 5, 6 };
+
+            // Declare a two dimensional array.
+            int[,] multiDimensionalArray1 = new int[2, 3];
+
+            // Declare and set array element values.
+            int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            // Declare a jagged array.
+            int[][] jaggedArray = new int[6][];
+
+            // Set the values of the first array in the jagged array structure.
+            jaggedArray[0] = new int[4] { 1, 2, 3, 4 };
+
+
+            int[,,] array1 = new int[4, 2, 3];
+
+
+            // Two-dimensional array.
+            int[,] array2D = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            // The same array with dimensions specified.
+            int[,] array2Da = new int[4, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            // A similar array with string elements.
+            string[,] array2Db = new string[3, 2] { { "one", "two" }, { "three", "four" },
+                                        { "five", "six" } };
+
+            // Three-dimensional array.
+            int[,,] array3D = new int[,,] { { { 1, 2, 3 }, { 4, 5, 6 } },
+                                 { { 7, 8, 9 }, { 10, 11, 12 } } };
+            // The same array with dimensions specified.
+            int[,,] array3Da = new int[2, 2, 3] { { { 1, 2, 3 }, { 4, 5, 6 } },
+                                       { { 7, 8, 9 }, { 10, 11, 12 } } };
+
+            // Accessing array elements.
+            System.Console.WriteLine(array2D[0, 0]);
+            System.Console.WriteLine(array2D[0, 1]);
+            System.Console.WriteLine(array2D[1, 0]);
+            System.Console.WriteLine(array2D[1, 1]);
+            System.Console.WriteLine(array2D[3, 0]);
+            System.Console.WriteLine(array2Db[1, 0]);
+            System.Console.WriteLine(array3Da[1, 0, 1]);
+            System.Console.WriteLine(array3D[1, 1, 2]);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
